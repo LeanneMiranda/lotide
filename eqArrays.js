02877503d23a9412ce62ec661e75ae0ae81f8680
LeanneMiranda <leannemiranda@gmail.com>
2022-10-24 21:14:41 -0700

    added comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+// assertEqual is supposed to compare 2 values (actual & expected) and if they match then give back a pretty msg and if they don't, a bad msg
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -5,15 +6,19 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+// eqArrays takes in 2 parameters (or arguments)
 const eqArrays = function(arr1, arr2) {
+  // compare the 2 arguments to see if they are the same length, if they don't match, then return false
   if (arr1.length !== arr2.length) {
     return false;
   }
+    // use a for loop to iterate through each array 1 at a time to compare if the value is the same in both arrays, if not the same, return false
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
+  // if we didn't trigger any of our if statements, then we would return true
   return true;
 };
 
