c8eec2f6a2738a48e3bf809fedb4979f67b37c8d
LeanneMiranda <leannemiranda@gmail.com>
2022-10-24 18:59:15 -0700

    added comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+// assertEqual is supposed to compare 2 values (actual & expected) and if they match then give back a pretty msg and if they don't, a bad msg
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
