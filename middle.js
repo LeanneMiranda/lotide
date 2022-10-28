af752e52c973a8646853a163ab85dff7059737fc
LeanneMiranda <leannemiranda@gmail.com>
2022-10-28 13:45:13 -0700

    added 'require' and 'module.exports'

--- middle.js
+++ middle.js
@@ -1,23 +1,5 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-    
-  return true;
-};
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 const middle = function(array) {
   if(array.length <= 1) {
@@ -30,10 +12,7 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1, 2, 3]),[2])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual("Lighthouse Labs", "Bootcamp");
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual([1, 2, 3], [1, 3, 3]);
-console.log(middle([1, 2, 4]));
+module.exports = middle;
+
+
 
